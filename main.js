1bc3ec6cab5d52b2f5b6ecd3079bf5bdd03dcc84
Jotony Guerra <jotonyguerra@gmail.com>
2017-06-09 12:21:09 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let inventory = [
+  ['LA-LG-STICKER', 300, 'Large Sticker', 'images/la-large-sticker-256px.png', 'Show your Launch pride by plastering your laptop with these beautiful die-cut stickers.'],
+  ['LA-SM-STICKER', 200, 'Small Sticker', 'images/la-small-sticker-128px.png', "It\'s a tiny Launch sticker. How cute!"],
+  ['LA-T-SHRIT', 50, 'T-Shirt', 'images/la-t-shirt-200px.png', '100% Cotton. Makes a great gift.']
+]
+
+// your code, here
